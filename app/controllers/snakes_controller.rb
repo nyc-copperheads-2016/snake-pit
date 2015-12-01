1c5ac7ee11165ad69e5f4d46d027e08cd8c7638d
Chris DiMartino <vwchris@gmail.com>
2015-12-01 12:04:30 -0500

    Styling

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -2,3 +2,16 @@ get '/' do
   @snakes = Snake.all
   erb :'snakes/index'
 end
+
+get '/snakes/new' do
+  erb :'snakes/form'
+end
+
+post '/snakes' do
+  snake = Snake.new(name: params[:name], species: params[:species])
+  if snake.save
+    redirect '/'
+  else
+    erb :'snakes/form'
+  end
+end
