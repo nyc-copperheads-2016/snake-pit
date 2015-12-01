bd7cc8d1684d1e273f88f3106779fe9cb2037210
Chris DiMartino <vwchris@gmail.com>
2015-12-01 14:59:47 -0500

    Delete a snake

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -1,10 +1,15 @@
 get '/' do
-  @snakes = Snake.all
-  erb :'snakes/index'
+  snakes = Snake.all
+  erb :'snakes/index', locals: { all_snakes: snakes }
 end
 
 get '/snakes/new' do
   erb :'snakes/form'
+end
+
+get '/snakes/:id' do
+  ## Find a snake
+  # erb the snake
 end
 
 post '/snakes' do
@@ -13,5 +18,14 @@ post '/snakes' do
     redirect '/'
   else
     erb :'snakes/form'
+  end
+end
+
+delete '/snakes/:id' do
+  snake = Snake.find_by(id: params[:id])
+  if snake.delete
+    redirect '/'
+  else
+    erb :'snakes/show_snake', locals: { errors: [ 'Could not delete snake' ] }
   end
 end
