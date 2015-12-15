3110431af9a5fc601993bf5b2295cf5f4a806483
Chris DiMartino <vwchris@gmail.com>
2015-12-15 09:13:42 -0500

    Showing how new/build/create relate

--- app/controllers/skins_contoller.rb
+++ app/controllers/skins_contoller.rb
@@ -1,9 +1,14 @@
 post '/snakes/:id/skins' do
   snake = Snake.find(params[:id])
+
+  # Snake.new    === snake.skins.build(params)
+  # Snake.create === snake.skins.create
+
   if snake.skins.create
     if request.xhr?
       content_type :json
-      { count: snake.skins.length }.to_json
+      # { count: snake.skins.length }
+      snake.to_json include: :skins
     else
       redirect '/'
     end
